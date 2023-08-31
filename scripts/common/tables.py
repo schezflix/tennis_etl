f45838abeda18f70270889366b88231a4434c743
schezflix <jasfschez@gmail.com>
2023-08-30 21:04:21 -0700

    added the cleaning csv feature to the extract part. Also enhanced the tables model by creating realtionships between them

--- scripts/common/tables.py
+++ scripts/common/tables.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Date, ForeignKey 
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey 
 from sqlalchemy.orm import relationship
 from base import Base
 
@@ -14,26 +14,26 @@ class Match(Base):
     # relationships
     players = relationship("Player", backref="fact_matches")
     tournaments = relationship("Tournament", backref="fact_matches")
+    time_dimension  = relationship("TimeDimension", back_populates="matches")
 
     
-    
-class Tournament(Base):
+class Tournament(Base): # pets
     __tablename__ = 'dim_tournaments'
     
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(255))
     category = Column(String(255))
-    location = Column(Integer, ForeignKey('dim_countries.id'))
-    surface = Column(Integer, ForeignKey('dim_surfaces.id'))
+    location_id = Column(Integer, ForeignKey('dim_countries.id'))
+    surface_id = Column(Integer, ForeignKey('dim_surfaces.id'))
 
 
-    winning_price = Column(Integer())
-    date = Column(Date)
+    winning_prize = Column(Integer(), nullable=True)
+    date = Column(DateTime(), nullable=False)
     
     
-class Surface(Base):
+class Surface(Base): # owner
     __tablename__ = 'dim_surfaces'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(255))
   
     # relationships
@@ -45,17 +45,35 @@ class Player(Base):
     
     id = Column(Integer, primary_key=True)
     complete_name = Column(String(255))
-    nationality = Column(Integer, ForeignKey('dim_countries.id'))
+    nationality_id = Column(Integer, ForeignKey('dim_countries.id'))
     
     
 class Country(Base):
     __tablename__ = 'dim_countries'
     
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(255))
+    region = Column(String(255))
+    sub_region = Column(String(255))
 
     # relationships
     players = relationship('Player', backref='dim_players')
     tournaments = relationship('Tournament', backref='dim_tournaments')
     
+    
+class TimeDimension(Base):
+    __tablename__ = 'dim_date'
+    
+    id = Column(Integer, primary_key=True)
+    date = Column(DateTime, unique=True)
+    year = Column(Integer)
+    month = Column(Integer)
+    day = Column(Integer)
+    day_of_week = Column(Integer)
+    quarter = Column(Integer)
+    is_weekend = Column(Integer)
+    
+    # relationships
+    matches = relationship("Matches", back_populates="time_dimension")
+
     
--- scripts/extract.py
+++ scripts/extract.py
@@ -1,11 +1,13 @@
 import requests, io, csv, os
 from zipfile import ZipFile
 from datetime import date
+import pandas as pd
 
 
 BASE_PATH = os.path.abspath('')
 SOURCE_PATH = f"{BASE_PATH}/data/source/tennis_csvs.zip"
 RAW_PATH = f"{BASE_PATH}/data/raw/"
+NEW_CSVS_PATH = f"{BASE_PATH}/data/csvs/"
 
 
 
@@ -25,9 +27,32 @@ def save_raw_data():
     with ZipFile(SOURCE_PATH, mode= 'r') as f:
         f.extractall(path=RAW_PATH)
         
+def save_new_csv_data():
+    """
+    Saves new csvs with only necessary columns
+    """
+    create_folder_if_not_exists(NEW_CSVS_PATH)
+    
+    # countries
+    pd.read_csv('data/raw/countries.csv', usecols=['name', 'code', 'region', 'sub_region']
+                , dtype={'name': str, 'region': str, 'sub_region': str})\
+        .to_csv(f'{NEW_CSVS_PATH}countries.csv', index=False)
+
+    # players
+    pd.read_csv('data/raw/all_players.csv'
+                , dtype={'player_id': str, 'country': str})\
+        .to_csv(f'{NEW_CSVS_PATH}players.csv', index=False)
         
+    # tournaments
+    pd.read_csv('data/raw/all_tournaments.csv', usecols=['tournament', 'location', 'court_surface', 'prize_money', 'masters']
+            , dtype={'tournament': str, 'location': str, 'court_surface': str, 'prize_money': str, 'masters': str})\
+        .to_csv(f'{NEW_CSVS_PATH}tournaments.csv', index=False)
+
+
 def main():
     print("[Extract] Start")
-    print(f"[Extract] Extracting data from '{SOURCE_PATH}' to '{RAW_PATH}'")
+    print(f"[Extract] Extracting data from '{SOURCE_PATH}' to '{RAW_PATH}' and adding a clean version into '{NEW_CSVS_PATH}'")
     save_raw_data()
+    save_new_csv_data()
     print(f"[Extract] End")
+    
